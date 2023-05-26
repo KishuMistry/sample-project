913b2e7d0bb121d6a819654d020c02a471af5007
Kishan.Mistry <kishan.mistry@trivium-esolutions.com>
2023-05-26 15:46:56 +0530

    Alert Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import PCMessage from "./Component/PCMessage";
+import AlertExample from "./Component/AlertExample";
 
 function App() {
   return (
     <div>
       <PCMessage text="Hello My First Property"></PCMessage>
+      <AlertExample />
     </div>
   );
 }
